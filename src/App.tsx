706410b67f24cbf1c7ca267690df9e81cc3ea4b0
s <s@localhost.localdomain>
2023-04-05 18:13:15 +0700

    add card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,63 @@
-import React from 'react';
+import { useEffect, useRef, useState, MutableRefObject} from "react";
 import { Header } from 'app/component/Layout/Header';
+import { Pokedex } from "app/component/Pokedex";
+import { fetchPokemonList } from "app/utils/api/fetchPokemonList";
+import { Pokemon } from "app/utils/types/Pokemon";
 
 
 function App() {
+  const [modal, setModal] = useState(false);
+  const [pokemonData, setPokemonData] = useState<Pokemon>();
+  const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
+  const [pokemonAmount, setPokemonAmount] = useState(9);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [showPagination, setShowPagination] = useState(true);
+  const [disabledButton, setDisabledButton] = useState(false);
+  // const searchBarRef = useRef<HTMLDivElement>(null);
+  const searchBarRef = useRef<HTMLDivElement>(null) as MutableRefObject<HTMLDivElement>;
+
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      setPokemonList(await fetchPokemonList(1));
+      setLoading(false);
+    })();
+  }, []);
+
+  useEffect(() => {
+    const html = document.documentElement;
+
+    modal
+      ? (html.style.overflow = "hidden")
+      : (html.style.overflow = "initial");
+  }, [modal]);
+
+  useEffect(() => {
+    setError(false);
+  }, [pokemonList]);
   return (
     <>
       <Header />
-    
+      <Pokedex
+        setModal={setModal}
+        setPokemonData={setPokemonData}
+        pokemonList={pokemonList}
+        setPokemonList={setPokemonList}
+        pokemonAmount={pokemonAmount}
+        setPokemonAmount={setPokemonAmount}
+        error={error}
+        loading={loading}
+        setLoading={setLoading}
+        page={page}
+        setPage={setPage}
+        showPagination={showPagination}
+        setShowPagination={setShowPagination}
+        searchBarRef={searchBarRef}
+        disabledButton={disabledButton}
+      />
     </>
   );
 }
---
+++ src/app/component/Pokedex/index.tsx
@@ -0,0 +1,89 @@
+import UsePagination from "../Pagination";
+import { PokemonCard } from "../PokemonCard";
+import * as C from "./styles";
+import { ReactComponent as AddIcon } from "app/assets/icon-add.svg";
+import { ReactComponent as UpArrowIcon } from "app/assets/icon-arrow-up.svg";
+import { Pokemon } from "app/utils/types/Pokemon";
+
+type PokedexProps = {
+    setModal: (value: boolean) => void;
+    setPokemonData: (data: Pokemon) => void;
+    pokemonList: Pokemon[];
+    setPokemonList: (data: Pokemon[]) => void;
+    pokemonAmount: number;
+    setPokemonAmount: (value: number) => void;
+    error: boolean;
+    loading: boolean;
+    setLoading: (value: boolean) => void;
+    page: number;
+    setPage: (value: number) => void;
+    showPagination: boolean;
+    setShowPagination: (value: boolean) => void;
+    disabledButton: boolean;
+    searchBarRef: React.MutableRefObject<HTMLDivElement>;
+  };
+
+  export const Pokedex = (props: PokedexProps) => {
+    if (props.error) return <>error</>;
+    else
+      return (
+        <C.Wrapper>
+          <div className="main-container">
+            {props.loading ? (
+              <>loading</>
+            ) : (
+              <C.PokemonList>
+                {props.pokemonList.map((pokemon) => (
+                  <PokemonCard
+                    key={pokemon.id}
+                    pokemon={pokemon}
+                    setModal={props.setModal}
+                    setPokemonData={props.setPokemonData}
+                  />
+                ))}
+              </C.PokemonList>
+            )}
+            {props.pokemonList.length > 1 &&
+              props.loading === false &&
+              props.showPagination === true && (
+                <UsePagination
+                  setPokemonList={props.setPokemonList}
+                  setLoading={props.setLoading}
+                  searchBarRef={props.searchBarRef}
+                  page={props.page}
+                  setPage={props.setPage}
+                />
+              )}
+            {props.pokemonList.length > 1 &&
+              props.loading === false &&
+              props.showPagination === false && (
+                <C.ButtonContainer>
+                  {props.pokemonAmount < 54 && (
+                    <button
+                      className="button"
+                      onClick={() =>
+                        props.setPokemonAmount(props.pokemonAmount + 9)
+                      }
+                      disabled={props.disabledButton ? true : false}
+                    >
+                      <AddIcon />
+                      Mostrar mais pokémons
+                    </button>
+                  )}
+  
+                  <button
+                    className="button"
+                    onClick={() => {
+                      window.scrollTo({
+                        top: props.searchBarRef.current.offsetTop - 56,
+                      });
+                    }}
+                  >
+                    <UpArrowIcon />
+                  </button>
+                </C.ButtonContainer>
+              )}
+          </div>
+        </C.Wrapper>
+      );
+  };
